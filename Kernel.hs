9fb39586d892150ab8e07cd947e4ccfddc2ccf29
Dmitry <DimonErmolaev1994@gmail.com>
2015-12-27 16:45:46 +0300

    Сделал возможность задания прямоугольной матрицы

--- Kernel.hs
+++ Kernel.hs
@@ -7,10 +7,6 @@ import System.Random
 import System.Environment
 import System.IO
 
-import Parser
-import SimpleParsers
-import ParseNumbers
-
 import Control.Monad(when)
 
 type Position = (Int, Int)
@@ -18,7 +14,7 @@ type FieldSize = Int
 
 data FieldElem = Opened Int | Closed Int | Founded
     deriving (Show, Eq)
-
+    
 isOpened :: FieldElem -> Bool    
 isOpened (Opened x) = True
 isOpened _ = False
@@ -30,31 +26,34 @@ isFounded _ = False
 isClosed :: FieldElem -> Bool    
 isClosed (Closed x) = True
 isClosed _ = False
-            
-getRandomPosition :: StdGen -> FieldSize -> (Position, StdGen)
-getRandomPosition gen size = let (i1, nGen) = (randomR (1,size) gen) in ((i1, fst $ randomR (1,size) nGen), 
-                                    snd $ randomR (1,size) nGen)
+ 
+setClosed :: Position -> Matrix FieldElem -> Matrix FieldElem
+setClosed pos@(i1, j1) matr = case getElem i1 j1 matr of 
+        (Opened el) -> setElem (Closed el) pos matr
+        (Closed el) -> matr
+        Founded -> matr
+ 
+getRandomPosition :: StdGen -> Int -> Int -> (Position, StdGen)
+getRandomPosition gen sizeC sizeR = let (i1, nGen) = (randomR (1,sizeC) gen) in ((i1, fst $ randomR (1,sizeR) nGen), 
+                                    snd $ randomR (1,sizeR) nGen)
 
-genMatrixPositions :: StdGen -> FieldSize -> [Position]
-genMatrixPositions generator size = foldl(\acc x -> mainFun acc x generator) [] [1..size^2]
+genMatrixPositions :: StdGen -> Int -> Int -> [Position]
+genMatrixPositions generator sizeC sizeR = foldl(\acc x -> mainFun acc x generator) [] [1..sizeC*sizeR]
     where
-        mainFun used cur curGen = let (pos, nGen) = getRandomPosition curGen size in if elem pos used then
+        mainFun used cur curGen = let (pos, nGen) = getRandomPosition curGen sizeC sizeR in if elem pos used then
                 mainFun used cur nGen else used ++ [pos]
 
 fillMatrix :: Matrix FieldElem -> [Position] -> Matrix FieldElem
 fillMatrix = help 1
     where   
-        help :: Int -> Matrix FieldElem -> [Position] -> Matrix FieldElem
         help _ matr [] = matr
         help cur matr (x:y:xs) = help (cur + 1) (setElem (Closed cur) y (setElem (Closed cur) x matr)) xs 
  
-generateFieldMatrix :: StdGen -> FieldSize -> Matrix FieldElem
-generateFieldMatrix gen size = fillMatrix (myZero size size) (genMatrixPositions gen size) 
-
+generateFieldMatrix :: StdGen -> Int -> Int -> Matrix FieldElem
+generateFieldMatrix gen sizeC sizeR = fillMatrix (myZero sizeC sizeR) (genMatrixPositions gen sizeC sizeR) 
 
 myZero :: Int -> Int -> Matrix FieldElem
 myZero colums rows = fromLists $ replicate colums (replicate rows Founded) 
-
   
 isAllFounded :: Matrix FieldElem -> Bool
 isAllFounded = null . dropWhile(== Founded) . toList 
@@ -64,9 +63,9 @@ openCard (i, j) matr
     | getElem i j matr == Founded = matr
     | isOpened $ getElem i j matr = matr
     | otherwise = let (Closed elem) = getElem i j matr in (setElem (Opened elem) (i, j) matr)
-        
+ 
 checkOpened :: (Position, Position) -> Matrix FieldElem -> Matrix FieldElem
-checkOpened (pos1, pos2) matr = if check pos1 pos2 matr then guess pos1 pos2 matr else  matr
+checkOpened (pos1, pos2) matr = if check pos1 pos2 matr then guess pos1 pos2 matr else matr
     where
         check (i1, j1) (i2, j2) matr = getElem i1 j1 matr == getElem i2 j2 matr && getElem i2 j2 matr /= Founded
-        guess pos1 pos2 matr = setElem Founded pos1 (setElem Founded pos2 matr)       
+        guess pos1 pos2 matr = setElem Founded pos1 (setElem Founded pos2 matr)
