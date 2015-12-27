e1f5e7045f107fa1bdef3685fa71294a934e1a96
Banannzza <lek-ostapenko@yandex.ru>
2015-12-27 17:43:26 +0300

    Добавлены константы : размеры окна, fps и цвет фона

--- Window.hs
+++ Window.hs
@@ -5,6 +5,16 @@ import System.Random
 import Data.Matrix
 
 
+width, height, offset :: Int
+width = 1024
+height = 768
+offset = 0
+
+background :: Color
+background = white
+
+fps :: Int
+fps = 60
 
 main = undefined
     
